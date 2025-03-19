c41a360b577afa39b498ebc42904623a9bd13e68
Ilya Belikov <belikovilya0@gmail.com>
2025-03-19 20:23:35 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/bookfinder/data/model/BookResponse.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.bookfinder.data.model
+
+data class BookResponse(
+    val item: List<BookItem>?
+)
+
+data class BookItem(
+    val id: String,
+    val bookInfo: BookInfo
+)
+
+data class BookInfo(
+    val title: String,
+    val author: List<String>?,
+    val publishedDate: String?,
+    val description: String?,
+    val linkImages: LinkImages?
+)
+
+data class LinkImages(
+    val picture: String?
+)
